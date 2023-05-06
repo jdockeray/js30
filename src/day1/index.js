16d3489e79f1f97181f117c3d5426e78da1e4bf5
James Dockeray <james.dockeray@red-badger.com>
2023-05-06 22:52:20 +0100

    feat: add commit hooks and eslint

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,8 +1,8 @@
 /**
  * @return {Function}
  */
-var createHelloWorld = function () {
-  return function (...args) {
+const createHelloWorld = function () {
+  return function () {
     return "Hello World";
   };
 };
